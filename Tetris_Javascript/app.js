ee3ad0f4f338b1c17c4045bbb53b6a4b6ca2fd0a
Devon Gaston <kendgast@ut.utm.edu>
2020-09-02 15:25:52 -0500

    Stored grid array and HTML elements in app.js

--- Tetris_Javascript/app.js
+++ Tetris_Javascript/app.js
@@ -1,11 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-  var iDiv = document.createElement('div');
-  iDiv.id = 'grid';
-  iDiv.className = 'grid';
-  document.body.appendChild(iDiv);
-  var innerDiv;
-  for (var i = 0; i < 200; i++) {
-    innerDiv = document.createElement('div');
-    iDiv.appendChild(innerDiv);
+  //Create grid
+  var iDiv = document.createElement('div')
+  iDiv.id = 'grid'
+  iDiv.className = 'grid'
+  document.body.appendChild(iDiv)
+
+  // Create blocks in grid
+  var innerDiv
+  for (let i = 0; i < 200; i++) {
+    innerDiv = document.createElement('div')
+    iDiv.appendChild(innerDiv)
   }
+
+  // Define and HTML elements (grid, squares, etc)
+  const grid = document.querySelector('.grid')
+  let squares = Array.from(document.querySelectorAll('.grid div'))
+  const ScoreDisplay = document.querySelector('#score')
+  const StartBtn = document.querySelector('#start-button')
+  const width = 10
+
 })
