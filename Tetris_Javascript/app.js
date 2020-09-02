22f354e48434516818404680c29320b6a5bdc921
Devon Gaston <kendgast@ut.utm.edu>
2020-09-02 14:48:50 -0500

    Created base HTML page for game

---
+++ Tetris_Javascript/app.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded' () => {
+
+})
+
+/*var iDiv = document.createElement('div');
+iDiv.id = "grid";
+iDiv.className = "grid";
+document.getElementByTagName('body')[0].appendChild(iDiv);
+var innerDiv;
+for(int i = 0; i < 200; i++){
+  innerDiv = document.createElement('div');
+  iDiv.appendChild(innerDiv);
+}*/
