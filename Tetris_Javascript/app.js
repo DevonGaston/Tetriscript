dbceed5fba22eafe49ee935d5e1b0e5f28ff2bb5
Devon Gaston <kendgast@ut.utm.edu>
2020-09-02 15:10:16 -0500

    Now divs are dynamically created in app.js file

--- Tetris_Javascript/app.js
+++ Tetris_Javascript/app.js
@@ -1,13 +1,11 @@
-document.addEventListener('DOMContentLoaded' () => {
-
+document.addEventListener('DOMContentLoaded', () => {
+  var iDiv = document.createElement('div');
+  iDiv.id = 'grid';
+  iDiv.className = 'grid';
+  document.body.appendChild(iDiv);
+  var innerDiv;
+  for (var i = 0; i < 200; i++) {
+    innerDiv = document.createElement('div');
+    iDiv.appendChild(innerDiv);
+  }
 })
-
-/*var iDiv = document.createElement('div');
-iDiv.id = "grid";
-iDiv.className = "grid";
-document.getElementByTagName('body')[0].appendChild(iDiv);
-var innerDiv;
-for(int i = 0; i < 200; i++){
-  innerDiv = document.createElement('div');
-  iDiv.appendChild(innerDiv);
-}*/
