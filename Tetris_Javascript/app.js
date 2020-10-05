c1b1144dbe5bb82400e820d24d113c380709b2a1
Devon Gaston <gastondevon@yahoo.com>
2020-10-04 23:24:10 -0500

    Implemented J tetronimo successfully

--- Tetris_Javascript/app.js
+++ Tetris_Javascript/app.js
@@ -50,19 +50,19 @@ document.addEventListener('DOMContentLoaded', () => {
   let canMove = 0
 
   // Tetriminos
-  const lTetromino = [
+  const lTetronimo = [
     [1, width + 1, width * 2 + 1, 2],
     [width, width + 1, width + 2, width * 2 + 2],
     [1, width + 1, width * 2 + 1, width * 2],
     [width, width * 2, width * 2 + 1, width * 2 + 2]
   ]
   const jTetronimo = [
-    [width * 2 + 1, width + 1, 1, width * 2 + 2],
-    [width, width + 1, width + 2, 1],
-    [width * 2, width, 1, 2],
-    [width, 0, 1, 2]
+    [0, 1, width + 1, width * 2 + 1],
+    [width, width + 1, width + 2, 2],
+    [1, width + 1, width * 2 + 1, width * 2 + 2],
+    [width * 2, width, width + 1, width + 2]
   ]
-  const zTetromino = [
+  const zTetronimo = [
     [0, width, width + 1, width * 2 + 1],
     [width + 1, width + 2, width * 2, width * 2 + 1],
     [0, width, width + 1, width * 2 + 1],
@@ -74,26 +74,26 @@ document.addEventListener('DOMContentLoaded', () => {
     [1, width + 1, width, width * 2],
     [width, width + 1, width * 2 + 1, width * 2 + 2]
   ]
-  const tTetromino = [
+  const tTetronimo = [
     [1, width, width + 1, width + 2],
     [1, width + 1, width + 2, width * 2 + 1],
     [width, width + 1, width + 2, width * 2 + 1],
     [1, width, width + 1, width * 2 + 1]
   ]
-  const oTetromino = [
+  const oTetronimo = [
     [0, 1, width, width + 1],
     [0, 1, width, width + 1],
     [0, 1, width, width + 1],
     [0, 1, width, width + 1]
   ]
-  const iTetromino = [
+  const iTetronimo = [
     [1, width + 1, width * 2 + 1, width * 3 + 1],
     [width, width + 1, width + 2, width + 3],
     [1, width + 1, width * 2 + 1, width * 3 + 1],
     [width, width + 1, width + 2, width + 3]
   ]
-  const theTetronimoes = [lTetromino, zTetromino, sTetronimo,
-    tTetromino, oTetromino, iTetromino]
+  const theTetronimoes = [lTetronimo, jTetronimo, zTetronimo, sTetronimo,
+    tTetronimo, oTetronimo, iTetronimo]
 
   let currentPosition = 4
   let currentRotation = 0
@@ -231,13 +231,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // theTetronimoes sans rotations
   const upNextTetronimo = [
-    [1, displayWidth + 1, displayWidth * 2 + 1, 2], // lTetromino
-  //  [displayWidth * 2 + 1, displayWidth + 1, 1, displayWidth * 2 + 2], // sTetromino
-    [0, displayWidth, displayWidth + 1, displayWidth * 2 + 1], // zTetromino
+    [1, displayWidth + 1, displayWidth * 2 + 1, 2], // lTetronimo
+    [0, 1, displayWidth + 1, displayWidth * 2 + 1], // jTetronimo
+    [0, displayWidth, displayWidth + 1, displayWidth * 2 + 1], // zTetronimo
     [1, displayWidth + 1, displayWidth, displayWidth * 2], // sTetronimo
-    [1, displayWidth, displayWidth + 1, displayWidth + 2], // tTetromino
-    [0, 1, displayWidth, displayWidth + 1], // oTetromino
-    [1, displayWidth + 1, displayWidth * 2 + 1, displayWidth * 3 + 1] // iTetromino
+    [1, displayWidth, displayWidth + 1, displayWidth + 2], // tTetronimo
+    [0, 1, displayWidth, displayWidth + 1], // oTetronimo
+    [1, displayWidth + 1, displayWidth * 2 + 1, displayWidth * 3 + 1] // iTetronimo
   ]
 
   // Display shape in mini-grid display
